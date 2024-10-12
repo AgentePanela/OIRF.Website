7988268edad34fba85a0fbce34b2990e2cc18f18
AgentePanela <agentepanela@gmail.com>
2024-10-11 22:40:17 -0300

    first commit

---
+++ dash/register.js
@@ -0,0 +1,28 @@
+document.getElementById('userForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    console.log("Connecting...")
+
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const messageDiv = document.getElementById('message');
+
+    try {
+        const response = await fetch('https://oirf.online/app/api/save-user', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, email, password }) 
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            messageDiv.innerHTML = `<p style="color: green;">${data.message}</p>`;
+        } else {
+            messageDiv.innerHTML = `<p style="color: red;">${data.error}</p>`;
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        messageDiv.innerHTML = `<p style="color: red;">Error connecting to the server!.</p>`;
+    }
+});
---
+++ navLoader.js
@@ -0,0 +1,5 @@
+// carrega a navbar
+$(document).ready(function(){
+    $("#navbar-repo").load("/navbar.html");
+    console.log("nav loaded");
+});
