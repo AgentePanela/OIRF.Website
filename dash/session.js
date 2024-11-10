933794c117e06e690eb3de2bffe06b7c54944816
DoutorWhite <thedoctorwhite@gmail.com>
2024-11-10 00:47:54 -0300

    Se isso não dar certo o jeito é cometer sudoku

---
+++ dash/session.js
@@ -0,0 +1,68 @@
+console.log("Session module enabled!")
+export async function refreshSession() {
+    const refreshToken = atob(localStorage.getItem("refreshToken"));
+
+    if(!refreshToken)
+        alert("You don't have an active session. Try logging in again, please.")
+
+    try {
+        const response = await fetch('https://oirf.online/app/api/users/refresh', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ refreshToken: refreshToken })
+        });
+		
+        const result = await response.json();
+
+        if (response.status === 200) {
+            localStorage.setItem("sessionToken", btoa(result.sessionToken));
+            return true;
+        } else if (response.status === 403) {
+            console.error(response.message);
+            console.log("Your session is invalid or has expired. Try logging in again, please.")
+            localStorage.removeItem("refreshToken");
+            return false;
+        }
+    } catch (error) {
+        data.code = -1
+        data.message = "Error connecting to the server!"
+        console.error('Error:', error);
+        return(data);
+    }
+}
+
+export async function isSessionValid() {
+    const sessionToken = atob(localStorage.getItem("sessionToken"));
+
+    const response = await fetch('https://oirf.online/app/api/user?fields=username', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ sessionToken: sessionToken })
+    });
+
+    if (response.status === 200)
+        return true;
+
+    return refreshToken();
+}
+
+export async function logout() {
+    if (!isSessionValid())
+        return;
+
+    const sessionToken = atob(localStorage.getItem("sessionToken"));
+    const refreshToken = atob(localStorage.getItem("refreshToken"));
+
+    const response = await fetch('https://oirf.online/app/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ sessionToken: sessionToken, refreshToken: refreshToken })
+    });
+
+    if (response.status === 200) {
+        alert("You logged out.")
+        localStorage.removeItem("sessionToken");
+        localStorage.removeItem("refreshToken");
+    }
+}
+
---
+++ dash/userApi.js
@@ -0,0 +1,30 @@
+async function getUserData(fields = [], token) {
+
+    const url = new URL('https://oirf.online/app/api/user');
+    if (fields.length > 0) {
+      url.searchParams.append('fields', fields.join(','));
+    }
+  
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`, 
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(`Error ${response.status}: ${errorData.message}`);
+      }
+  
+      const data = await response.json();
+      console.log('User data:', data);
+      return data;
+    } catch (error) {
+      console.error('Error fetching user data:', error.message);
+    }
+}
+
+  
