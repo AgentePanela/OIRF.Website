1c833a4a5f69a2426bbdf0fa4bb91451f9876a27
AgentePanela <agentepanela@gmail.com>
2025-04-18 19:51:49 -0300

    update url protocol redirect

--- dash/userApi.js
+++ dash/userApi.js
@@ -20,7 +20,7 @@ export async function getUserData(fields = [], token) {
       }
   
       const data = await response.json();
-      console.log('User data:', data);
+      //console.log('User data:', data);
       return data;
     } catch (error) {
       console.error('Error fetching user data:', error.message);
