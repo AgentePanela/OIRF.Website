01dd541b69dfffe3ea63596868e66834a3c17536
AgentePanela <agentepanela@gmail.com>
2025-04-25 16:54:54 -0300

    old style is back

--- navLoader.js
+++ navLoader.js
@@ -3,19 +3,24 @@ $(document).ready(function(){
     $("#navbar-repo").load("/navbar.html");
     console.log("nav loaded");
 
+    if (localStorage.getItem("oldStyle") == "true") {
+        const css = document.querySelector('link[rel="stylesheet"]')
+        css.href = "/../media/oldStyle.css";
+    }
+
     // cowntdown
-    if (localStorage.getItem("countdown") == 'false')
-        return;
-    const countdownDiv = `
-        <div class="main countdownDiv" style="width: 100%;">
-            <div class="countdownLabel">
-                <h2>OIRF Public Playtest in:</h2>
+    if (localStorage.getItem("countdown") == 'true') {
+        const countdownDiv = `
+            <div class="main countdownDiv" style="width: 100%;">
+                <div class="countdownLabel">
+                    <h2>OIRF Public Playtest in:</h2>
+                </div>
+                <p id="countdown" class="countdown">00:00:00</p>
             </div>
-            <p id="countdown" class="countdown">00:00:00</p>
-        </div>
-        <script src="/countdown.js"></script>
-        <script src="/mainModule.js"></script>
-    `;
-    
-    $("#navbar-repo").after(countdownDiv);
+            <script src="/countdown.js"></script>
+            <script src="/mainModule.js"></script>
+        `;
+        
+        $("#navbar-repo").after(countdownDiv);
+    }
 });
